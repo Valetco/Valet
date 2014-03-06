7039a30731d078c85c3447171b388416632fb7f2
brightsider <VladGetman@Brightside.local>
2014-03-06 21:13:22 +0200

    Initial Commit

---
+++ Valet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Valet
+//
+//  Created by Vlad Getman on 06.03.14.
+//  Copyright (c) 2014 Vlad Getman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ValetTests/ValetTests.m
@@ -0,0 +1,34 @@
+//
+//  ValetTests.m
+//  ValetTests
+//
+//  Created by Vlad Getman on 06.03.14.
+//  Copyright (c) 2014 Vlad Getman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ValetTests : XCTestCase
+
+@end
+
+@implementation ValetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
